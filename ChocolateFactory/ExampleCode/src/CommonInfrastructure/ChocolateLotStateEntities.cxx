ceae08cb7f0fe73d6aa14a2fc1082100230691a2
@angelrti <angel@rti.com>
2014-08-13 18:01:13 +0200

    Updated copyright notice in ChocolateFactory example

--- ChocolateFactory/ExampleCode/src/CommonInfrastructure/ChocolateLotStateEntities.cxx
+++ ChocolateFactory/ExampleCode/src/CommonInfrastructure/ChocolateLotStateEntities.cxx
@@ -1,12 +1,13 @@
-/*********************************************************************************************
-(c) 2005-2013 Copyright, Real-Time Innovations, Inc.  All rights reserved.    	                             
-RTI grants Licensee a license to use, modify, compile, and create derivative works 
-of the Software.  Licensee has the right to distribute object form only for use with RTI 
-products.  The Software is provided “as is”, with no warranty of any type, including 
-any warranty for fitness for any purpose. RTI is under no obligation to maintain or 
-support the Software.  RTI shall not be liable for any incidental or consequential 
-damages arising out of the use or inability to use the software.
-**********************************************************************************************/
+/*******************************************************************************
+ (c) 2005-2014 Copyright, Real-Time Innovations, Inc.  All rights reserved.
+ RTI grants Licensee a license to use, modify, compile, and create derivative
+ works of the Software.  Licensee has the right to distribute object form only
+ for use with RTI products.  The Software is provided "as is", with no warranty
+ of any type, including any warranty for fitness for any purpose. RTI is under
+ no obligation to maintain or support the Software.  RTI shall not be liable for
+ any incidental or consequential damages arising out of the use or inability to
+ use the software.
+ ******************************************************************************/
 #include "ChocolateLotStateEntities.h"
 #include "EnumPrintHelpers.h"
 
@@ -461,7 +462,7 @@ std::vector< DdsAutoType<ChocolateLotState> > *lots)
 				// Return a value of true that lot updates have been received
 				haveLotUpdates = true;
 
-				// Making copies of this type for clean API because we do not need 
+				// Making copies of this type for clean API because we do not need
 				// lowest latency for lot updates
 				DdsAutoType<ChocolateLotState> lot = lotStateUpdates[i];
 				lots->push_back(lot);
