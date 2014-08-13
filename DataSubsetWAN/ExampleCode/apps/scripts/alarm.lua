8978bd3ec9772e10e5f52d346b322c4939069880
@angelrti <angel@rti.com>
2014-08-13 18:07:55 +0200

    Updated copyright notice in DataSubsetWAN example

--- DataSubsetWAN/ExampleCode/apps/scripts/alarm.lua
+++ DataSubsetWAN/ExampleCode/apps/scripts/alarm.lua
@@ -1,5 +1,5 @@
 --*****************************************************************************
---*    (c) 2005-2013 Copyright, Real-Time Innovations, All rights reserved.   *
+--*    (c) 2005-2014 Copyright, Real-Time Innovations, All rights reserved.   *
 --*                                                                           *
 --*         Permission to modify and use for internal purposes granted.       *
 --* This software is provided 'as is', without warranty, express or implied.  *
